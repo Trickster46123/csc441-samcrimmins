788b220084aaf9d2ef181b693a32f2622b5b1e95
Sam Crimmins <samcrimm@yahoo.com>
2026-09-09 18:44:09 -0400

    Week 3 Wednesday Lab

--- csc441-kotlin/src/Week3Wed/Lab.kt
+++ csc441-kotlin/src/Week3Wed/Lab.kt
@@ -1,4 +1,29 @@
 package Week3Wed
 
-class Lab {
-}
+    fun main() {
+        val firstName = "Sam"
+        val major = "Computer Science"
+        val favoriteFood = "Venison Rice Bowl"
+
+        var cscCoursesTaken =  8
+        cscCoursesTaken += 1
+
+        val hometown: String = "Johnson City"
+        val age: Int = 22
+        val temperature: Double = 72.5
+        val likesKotlin: Boolean = true
+
+
+        println("$firstName is majoring in $major")
+        println("Next year I will be ${age + 1}")
+        println("My name has ${firstName.length} letters")
+
+        print("What is your favorite class? ")
+        val favoriteClass = readln()
+        println("$favoriteClass sounds like a good class.")
+        println("My favorite food is $favoriteFood")
+        println("I have taken $cscCoursesTaken CSC courses")
+        println("The temperature is $temperature degrees")
+        println("Do I like Kotlin? $likesKotlin")
+    }
+
